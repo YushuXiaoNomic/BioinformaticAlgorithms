99651b6c7e454f67900e069048bdc5df9132bdc0
Yushu Xiao <yushu.xiao@nomic.bio>
2023-01-27 17:15:29 -0500

    working on backtracking

--- PartialDigestionBackTrack.py
+++ PartialDigestionBackTrack.py
@@ -1,8 +1,8 @@
-# This only gives one possible solution
-def partial_digest_recur(frags, points):
+# This should gives all possible solutions
+def partial_digest_recur(frags, points,previous_frags):
     points.sort()
     if len(frags) == 0:
-        return points
+        return points,previous_frags
     frags.sort()
     longest_frag = frags[-1]
     new_ptsA = points.copy()
@@ -12,7 +12,7 @@ def partial_digest_recur(frags, points):
     if set(frags_A) <= set(frags):
         for frag in frags_A:
             frags.remove(frag)
-        return partial_digest_recur(frags, new_ptsA)
+        return partial_digest_recur(frags, new_ptsA,frags_A)
 
     new_ptsB = points.copy()
     ptB = points[-1] - longest_frag
@@ -22,14 +22,20 @@ def partial_digest_recur(frags, points):
     if set(frags_B) <= set(frags):
         for frag in frags_B:
             frags.remove(frag)
-        return partial_digest_recur(frags, new_ptsB)
+        return partial_digest_recur(frags, new_ptsB,frags_B)
 
 
 def partial_digest(digested_frags):
     digested_frags.sort()
     pts = [0, digested_frags[-1]]
     digested_frags.remove(digested_frags[-1])
-    return partial_digest_recur(digested_frags, pts)
+    solutions = []
+    solution,prev_frag = partial_digest_recur(digested_frags, pts)
+    if not solution in solutions:
+        solutions.append(solution)
+        del(solution[-2])
+        partial_digest_recur(prev_frag,solution,[])
+
 
 
 L = [2,2,3,3,4,5,6,7,8,10]
